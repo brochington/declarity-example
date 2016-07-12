3245682259841847af20f152c3b9c4a4482c15ca
Broch Stilley <broch@lumi.com>
2016-07-11 22:25:00 -0700

    Getting jsx pragma working.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 import declarity from 'declarity';
-console.log("declarity!!!!");
+
+console.log(declarity);
+const {Entity, register} = declarity;
+
+
+class Engine extends Entity {
+
+}
+
+class Scene extends Entity {
+    yoyo = 'yoyoyoyo'
+}
+
+class Camera extends Entity {
+
+}
+
+const stuff = (
+    <Engine something="here">
+        <Scene myScene="awesome">
+            <Camera />
+        </Scene>
+    </Engine>
+)
+
+register();

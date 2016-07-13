c2b55a9fc5b51bb9553bd6001b552b7cc8943ea8
Broch Stilley <broch@lumi.com>
2016-07-13 00:15:29 -0700

    Adding some test entities for testing.

--- src/index.js
+++ src/index.js
@@ -5,23 +5,60 @@ const {Entity, register} = declarity;
 
 
 class Engine extends Entity {
+    static actions = {
+        engineTestAction: () => {
+            console.log('engineTestAction!!');
+        }
+    }
 
+    willMount = () => {
+        // console.log('willMount Engine');
+    }
+
+    didMount = () => {
+        // console.log('didMount Engine');
+    }
 }
 
 class Scene extends Entity {
-    yoyo = 'yoyoyoyo'
+    static actions = {
+        getSomething: () => {
+            console.log('getSomething!!!!');
+        }
+    }
+
+    willMount = () => {
+        // console.log('willMount Scene');
+    }
+
+    didMount = () => {
+        // console.log('didMount Scene');
+    }
+}
+
+class SomethingSmall extends Entity {
+    willMount = () => {
+        console.log('SomethingSmall mount');
+    }
 }
 
 class Camera extends Entity {
-
+    render() {
+        return (
+            <SomethingSmall />
+        )
+    }
 }
 
 const stuff = (
     <Engine something="here">
         <Scene myScene="awesome">
             <Camera />
+            {[1, 2, 3, 4].map(i => <Camera />)}
         </Scene>
     </Engine>
-)
+);
 
-register();
+console.log("stuff", stuff);
+
+register(stuff);
